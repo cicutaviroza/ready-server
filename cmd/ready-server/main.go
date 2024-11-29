2c5732f4e79f25ea9a9e5e9ee82f1478f13bc7ab
Ivan Likhatskikh <donotpretend.yandex.ru>
2024-11-29 17:08:20 +0300

    Add routing

--- cmd/ready-server/main.go
+++ cmd/ready-server/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+
+	"ready-server/internal/handler"
+)
+
+func main() {
+	h := handler.NewHandler()
+
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Letsgo)
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ready-server
 
 go 1.23.3
+
+require github.com/go-chi/chi/v5 v5.1.0
